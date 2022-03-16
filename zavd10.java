7e712d3e110bdd74d5b1a1818fabc068ef16bb3f
loto4uck <101703715+loto4uck@users.noreply.github.com>
2022-03-16 11:52:50 +0200

    Add files via upload

---
+++ zavd10.java
@@ -0,0 +1,23 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class zavd10 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int x1 = sc.nextInt();
+		  int x2 = sc.nextInt();
+		  int y1 = sc.nextInt();
+		  int y2 = sc.nextInt();
+		  int x3 = x1 - x2;
+		  int y3 = y1 - y2;
+		  if ((-1 <= x3) && (x3 <= 1)){
+		  if ((-1 <= y3) && (y3 <= 1)){
+		  System.out.println("Yes");
+		  }else{
+		  System.out.println("No");
+		  }
+		  }
+		  }
+
+}
