a6b238c5c3f7ba7b134bedfd56193c0ef804332d
loto4uck <101703715+loto4uck@users.noreply.github.com>
2022-03-16 11:48:11 +0200

    Add files via upload

---
+++ zavd7.java
@@ -0,0 +1,19 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class zavd7 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a1 = sc.nextInt();
+		int a2 = sc.nextInt();
+		int b1 = sc.nextInt();
+		int b2 = sc.nextInt();
+		if ((a1 == b1) || (a2 == b2)){
+			System.out.println("Yes");
+			}else{
+				System.out.println("No");
+				}
+			}
+
+}
