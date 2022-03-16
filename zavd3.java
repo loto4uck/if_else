460832cdd7a164819fcae60f4d7a9e003775bf89
loto4uck <101703715+loto4uck@users.noreply.github.com>
2022-03-16 11:41:49 +0200

    Add files via upload

---
+++ zavd3.java
@@ -0,0 +1,21 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class zavd3 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		if ((x == y) || (x / 11 == 1)) {
+			System.out.println("YES");
+		}else {
+			if ((x > 1) && (y > 1)) {
+				System.out.println("YES");
+			}else {
+			System.out.println("NO");
+			}
+		}
+		}
+
+}
