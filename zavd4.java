1a948854ebd01d1399a158d79814a87662af7e8c
loto4uck <101703715+loto4uck@users.noreply.github.com>
2022-03-16 11:42:54 +0200

    Add files via upload

---
+++ zavd4.java
@@ -0,0 +1,20 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class zavd4 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		if (x > 0) {
+			System.out.println("1");
+		}else {
+			if(x == 0) {
+				System.out.println("0");
+			}else {
+				System.out.println("-1");
+			}
+			}
+		}
+
+}
